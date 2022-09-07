428683a0ac4569149c8f35a7d643c2dbbb154185
dinhdjj <dinhdjj@gmail.com>
2022-09-07 22:15:58 +0700

    feat: search for category list

--- views/category/index.php
+++ views/category/index.php
@@ -31,6 +31,14 @@ $renderCategoryRow = function ($category, $level = 0) use (&$renderCategoryRow) 
 </head>
 <body>
 
+    <!-- Search -->
+    <nav class="navbar navbar-light bg-light">
+        <form class="form-inline">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </nav>
+
     <!-- List categories -->
     <table class="table">
         <thead class="thead-dark">
